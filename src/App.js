29ae2d018b1f3395c82eab6556ce52c1a7cf51b9
bouizerguane <m.bouizerguane5118@uca.ac.ma>
2025-12-02 21:08:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CompteList from "./components/CompteList";
+import CompteForm from "./components/CompteForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CompteForm />
+      <CompteList />
     </div>
   );
 }
